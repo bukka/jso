b34853819b660ea7c68e1f17a7a3c7029048a8e9
Jakub Zelenka <bukka@php.net>
2015-01-18 19:53:58 +0000

    Add value switch to encoder

--- src/jso_encoder.c
+++ src/jso_encoder.c
@@ -26,7 +26,39 @@
 #include <stdio.h>
 #include <string.h>
 #include "jso.h"
+#include "jso_io.h"
 #include "jso_encoder.h"
+
+static int jso_encoder_output(jso_encoder *encoder, const char *str, size_t len)
+{
+	return JSO_IO_WRITE(encoder->output, (jso_ctype *) str, len);
+}
+
+static int jso_encoder_encode_int(jso_encoder *encoder, jso_int val)
+{
+	return 0;
+}
+
+static int jso_encoder_encode_double(jso_encoder *encoder, jso_double val)
+{
+	return 0;
+}
+
+static int jso_encoder_encode_string(jso_encoder *encoder, jso_string *str)
+{
+	return 0;
+}
+
+static int jso_encoder_encode_array(jso_encoder *encoder, jso_array *arr)
+{
+	return 0;
+}
+
+static int jso_encoder_encode_object(jso_encoder *encoder, jso_object *obj)
+{
+	return 0;
+}
+
 
 JSO_API void jso_encoder_init(jso_encoder *encoder, jso_io *output, jso_encoder_options *options)
 {
@@ -36,7 +68,28 @@ JSO_API void jso_encoder_init(jso_encoder *encoder, jso_io *output, jso_encoder_
 
 JSO_API int jso_encoder_encode(jso_encoder *encoder, jso_value *val)
 {
-	return 1;
+	switch (JSO_TYPE_P(val)) {
+		case JSO_TYPE_NULL:
+			return jso_encoder_output(encoder, "null", sizeof("null") - 1);
+		case JSO_TYPE_BOOL:
+			if (JSO_IVAL_P(val)) {
+				return jso_encoder_output(encoder, "true", sizeof("true") - 1);
+			}
+			return jso_encoder_output(encoder, "false", sizeof("false") - 1);
+		case JSO_TYPE_INT:
+			return jso_encoder_encode_int(encoder, JSO_IVAL_P(val));
+		case JSO_TYPE_DOUBLE:
+			return jso_encoder_encode_double(encoder, JSO_DVAL_P(val));
+		case JSO_TYPE_STRING:
+			return jso_encoder_encode_string(encoder, &val->data.str);
+		case JSO_TYPE_ARRAY:
+			return jso_encoder_encode_array(encoder, JSO_ARRVAL_P(val));
+		case JSO_TYPE_OBJECT:
+			return jso_encoder_encode_object(encoder, JSO_OBJVAL_P(val));
+		default:
+			/* error */
+			return -1;
+	}
 }
 
 JSO_API int jso_encode(jso_value *val, jso_io *output, jso_encoder_options *options)
