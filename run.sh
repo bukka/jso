6d6ee3da73c11b130b9b0707eed210037dae0806
Jakub Zelenka <bukka@php.net>
2015-06-24 19:24:13 +0100

    Tabify run.sh

--- run.sh
+++ run.sh
@@ -2,24 +2,25 @@
 
 # set base directory
 if readlink ${BASH_SOURCE[0]} > /dev/null; then
-  JSOBASE="$( dirname "$( readlink ${BASH_SOURCE[0]} )" )"
+	JSOBASE="$( dirname "$( readlink ${BASH_SOURCE[0]} )" )"
 else  
-  JSOBASE="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+	JSOBASE="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 fi
 
 if [[ "$1" == "-v" ]]; then
-  PREF=valgrind
-  shift
+	PREF=valgrind
+	shift
 fi
 
 if [ -n "$1" ]; then
-  TDIRS="$@"
+	TDIRS="$@"
 else
-  TDIRS=tests
+	TDIRS=tests
 fi
 
 for TFILE in `find $TDIRS -name '*.json'`; do
-  echo "---------------------------------"
-  echo $TFILE
-  $PREF $JSOBASE/src/jso $TFILE
+	echo "---------------------------------"
+	echo $TFILE
+	$PREF $JSOBASE/src/jso $TFILE
+	echo ""
 done
