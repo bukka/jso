1ce7479a204acf12c9f8162e571d2798dd417961
Jakub Zelenka <bukka@php.net>
2015-10-21 18:42:16 +0100

    Document jso_encoder

--- src/jso_encoder.h
+++ src/jso_encoder.h
@@ -21,31 +21,84 @@
  *
  */
 
+/**
+ * @file jso_encoder.h
+ * @brief Encoder
+ */
+
 #ifndef JSO_ENCODER_H
 #define JSO_ENCODER_H
 
 #include "jso_types.h"
 #include "jso_io.h"
 
+/**
+ * Constant value for unlimited depth
+ */
 #define JSO_ENCODER_DEPTH_UNLIMITED -1
 
+/**
+ * Indent string for pretty printing
+ */
 #define JSO_ENCODER_PRETTY_INDENT "    "
+
+/**
+ * Elements separator string for pretty printing
+ */
 #define JSO_ENCODER_PRETTY_ELSEP  "\n"
+
+/**
+ * Key-value separator for pretty printing
+ */
 #define JSO_ENCODER_PRETTY_KVSEP  " "
 
+/**
+ * @brief Option for encoder
+ */
 typedef struct _jso_encoder_options {
+	/** maximal depth that can be encoded */
 	jso_int max_depth;
+	/** whether to use pretty printing */
 	jso_bool pretty;
 } jso_encoder_options;
 
+/**
+ * @brief Encoder instance handle structure
+ */
 typedef struct _jso_encoder {
+	/** encoder output IO */
 	jso_io *output;
+	/** encoder options */
 	jso_encoder_options options;
+	/* actual depth */
 	jso_int depth;
 } jso_encoder;
 
-JSO_API void jso_encoder_init(jso_encoder *encoder, jso_io *output, jso_encoder_options *options);
+/**
+ * Initialize encoder
+ * @param encoder encoder instance
+ * @param output output IO
+ * @param options options
+ */
+JSO_API void jso_encoder_init(jso_encoder *encoder,
+        jso_io *output, jso_encoder_options *options);
+
+/**
+ * Encode value using the encoder
+ * @param encoder encoder instance
+ * @param val value to encode
+ * @return @ref JSO_SUCCESS on success, otherwise @ref JSO_FAILURE.
+ */
 JSO_API jso_rc jso_encoder_encode(jso_encoder *encoder, jso_value *val);
-JSO_API jso_rc jso_encode(jso_value *val, jso_io *output, jso_encoder_options *options);
+
+/**
+ * Encode supplied value
+ * @param val value to encode
+ * @param output output IO
+ * @param options options
+ * @return @ref JSO_SUCCESS on success, otherwise @ref JSO_FAILURE.
+ */
+JSO_API jso_rc jso_encode(jso_value *val,
+        jso_io *output, jso_encoder_options *options);
 
 #endif /* JSO_ENCODER_H */
