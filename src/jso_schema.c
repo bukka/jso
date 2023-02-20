d6b9762430d1415f04f1423b842ca0a2e52f6f01
Jakub Zelenka <bukka@php.net>
2023-02-20 15:51:00 +0000

    Add initial schema implementation file

---
+++ src/jso_schema.c
@@ -0,0 +1,53 @@
+/*
+ * Copyright (c) 2023 Jakub Zelenka. All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ *
+ */
+
+#include "jso.h"
+#include "jso_schema.h"
+
+#include <stdlib.h>
+#include <string.h>
+
+JSO_API jso_schema *jso_schema_alloc()
+{
+	return jso_calloc(1, sizeof(jso_schema));
+}
+
+JSO_API void jso_schema_init(jso_schema *schema)
+{
+	memset(schema, 0, sizeof(jso_schema));
+}
+
+JSO_API jso_rc jso_schema_parse(jso_schema *schema, jso_value *data)
+{
+    return JSO_SUCCESS;
+}
+
+JSO_API void jso_schema_clear(jso_schema *schema)
+{
+}
+
+JSO_API void jso_schema_free(jso_schema *schema)
+{
+	jso_schema_clear(schema);
+	jso_free(schema);
+}
