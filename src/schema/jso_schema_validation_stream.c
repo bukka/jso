ff4987aed230db53884aa3252d8a0a2b443299b9
Jakub Zelenka <bukka@php.net>
2024-12-30 11:52:01 +0100

    Add comments to describe validation stream logic

--- src/schema/jso_schema_validation_stream.c
+++ src/schema/jso_schema_validation_stream.c
@@ -45,9 +45,11 @@ JSO_API jso_rc jso_schema_validation_stream_init(
 	JSO_ASSERT_GE(stack_capacity, 1);
 	jso_schema_validation_stack *stack = JSO_STREAM_VALIDATION_STREAM_STACK_P(stream);
 
+	// Initialize validation stack
 	if (jso_schema_validation_stack_init(schema, stack, stack_capacity) == JSO_FAILURE) {
 		return JSO_FAILURE;
 	}
+	// Root element needs to be always pushed.
 	if (jso_schema_validation_stack_push_basic(stack, schema->root, NULL) == NULL) {
 		jso_schema_validation_stream_clear(stream);
 		return JSO_FAILURE;
@@ -63,6 +65,7 @@ JSO_API jso_rc jso_schema_validation_stream_object_start(jso_schema_validation_s
 	jso_schema_validation_stack *stack = JSO_STREAM_VALIDATION_STREAM_STACK_P(stream);
 	jso_schema *schema = stack->root_schema;
 
+	// Iterate to do a composition check and initial type check.
 	jso_schema_validation_stack_layer_iterator_start(stack, &iterator);
 	while ((pos = jso_schema_validation_stack_layer_iterator_next(stack, &iterator))) {
 		jso_schema_value *value = pos->current_value;
@@ -87,19 +90,25 @@ JSO_API jso_rc jso_schema_validation_stream_object_key(
 	jso_schema_validation_stack *stack = JSO_STREAM_VALIDATION_STREAM_STACK_P(stream);
 	jso_schema *schema = stack->root_schema;
 
+	// Start parent iteration.
 	jso_schema_validation_stack_layer_iterator_start(stack, &iterator);
+	// Push the stack so schemas for the key property is pushed to the new layer.
 	if (jso_schema_validation_stack_push_separator(stack) == NULL) {
 		return JSO_FAILURE;
 	}
+	// Iterate through all positions.
 	while ((pos = jso_schema_validation_stack_layer_iterator_next(stack, &iterator))) {
+		// Keep count of object elements.
 		++pos->count;
 		if (pos->validation_result == JSO_SCHEMA_VALIDATION_VALID
 				&& JSO_SCHEMA_VALUE_TYPE_P(pos->current_value) == JSO_SCHEMA_VALUE_TYPE_OBJECT) {
+			// Validate object key for valid object schemas - add schema for properties and others
 			pos->validation_result = jso_schema_validation_object_key(stack, pos, key);
 			if (jso_schema_validation_stream_should_terminate(schema, pos)) {
 				return JSO_FAILURE;
 			}
 		}
+		// Remember the current position key.
 		pos->object_key = key;
 	}
 
@@ -109,13 +118,13 @@ JSO_API jso_rc jso_schema_validation_stream_object_key(
 JSO_API jso_rc jso_schema_validation_stream_object_update(jso_schema_validation_stream *stream,
 		jso_object *instance_object, jso_string *instance_key, jso_value *instance_item)
 {
-	// currently there is nothing to do
+	// Currently there is nothing to do.
 	return JSO_SUCCESS;
 }
 
 JSO_API jso_rc jso_schema_validation_stream_object_end(jso_schema_validation_stream *stream)
 {
-	// currently there is nothing to do
+	// Currently there is nothing to do.
 	return JSO_SUCCESS;
 }
 
@@ -126,6 +135,7 @@ JSO_API jso_rc jso_schema_validation_stream_array_start(jso_schema_validation_st
 	jso_schema_validation_stack *stack = JSO_STREAM_VALIDATION_STREAM_STACK_P(stream);
 	jso_schema *schema = stack->root_schema;
 
+	// Iterate to do a composition check and initial type check.
 	jso_schema_validation_stack_layer_iterator_start(stack, &iterator);
 	while ((pos = jso_schema_validation_stack_layer_iterator_next(stack, &iterator))) {
 		jso_schema_value *value = pos->current_value;
@@ -140,19 +150,24 @@ JSO_API jso_rc jso_schema_validation_stream_array_start(jso_schema_validation_st
 		}
 	}
 
+	// Start next iteration round in the parent.
 	jso_schema_validation_stack_layer_iterator_start(stack, &iterator);
+	// Push separator so the schema for the item at the current index is added to the new layer.
 	if (jso_schema_validation_stack_push_separator(stack) == NULL) {
 		return JSO_FAILURE;
 	}
+	// Iterate to add array start positions.
 	while ((pos = jso_schema_validation_stack_layer_iterator_next(stack, &iterator))) {
 		if (pos->validation_result == JSO_SCHEMA_VALIDATION_VALID) {
 			jso_schema_value *value = pos->current_value;
 			if (JSO_SCHEMA_VALUE_TYPE_P(value) == JSO_SCHEMA_VALUE_TYPE_ARRAY) {
+				// It adds position for the first item in the array except other things.
 				jso_schema_validation_result result = jso_schema_validation_array_start(stack, pos);
 				if (result == JSO_SCHEMA_VALIDATION_ERROR) {
 					return JSO_FAILURE;
 				}
 			} else if (JSO_SCHEMA_VALUE_TYPE_P(value) != JSO_SCHEMA_VALUE_TYPE_MIXED) {
+				// Make sure that non array or mixed types fail.
 				pos->validation_result = jso_schema_validation_value_type_error(
 						schema, JSO_SCHEMA_VALUE_TYPE_P(value), JSO_SCHEMA_VALUE_TYPE_ARRAY);
 				jso_schema_validation_result_propagate(schema, pos);
@@ -170,13 +185,18 @@ JSO_API jso_rc jso_schema_validation_stream_array_append(
 	jso_schema_validation_position *pos;
 	jso_schema_validation_stack *stack = JSO_STREAM_VALIDATION_STREAM_STACK_P(stream);
 
+	// Start iteration round in the parent.
 	jso_schema_validation_stack_layer_iterator_start(stack, &iterator);
+	// Push separator so the schema for the item at the current index is added to the new layer.
 	if (jso_schema_validation_stack_push_separator(stack) == NULL) {
 		return JSO_FAILURE;
 	}
 	jso_schema *schema = stack->root_schema;
 	while ((pos = jso_schema_validation_stack_layer_iterator_next(stack, &iterator))) {
+		// Count number of items.
 		++pos->count;
+		// The array append is called only for valid array schema values, and it adds schema for the
+		// next item.
 		if (JSO_SCHEMA_VALUE_TYPE_P(pos->current_value) == JSO_SCHEMA_VALUE_TYPE_ARRAY
 				&& jso_schema_validation_array_append(stack, pos) != JSO_SCHEMA_VALIDATION_VALID) {
 			if (jso_schema_validation_stream_should_terminate(schema, pos)) {
@@ -191,6 +211,7 @@ JSO_API jso_rc jso_schema_validation_stream_array_append(
 
 JSO_API jso_rc jso_schema_validation_stream_array_end(jso_schema_validation_stream *stream)
 {
+	// Remove layer for previously added schema in the last append.
 	jso_schema_validation_stack_layer_remove(JSO_STREAM_VALIDATION_STREAM_STACK_P(stream));
 	return JSO_SUCCESS;
 }
@@ -204,11 +225,14 @@ JSO_API jso_rc jso_schema_validation_stream_value(
 	jso_schema *schema = stack->root_schema;
 
 	jso_value_type instance_type = JSO_TYPE_P(instance);
+	// Array has already added composition during array start so skip it.
 	if (instance_type != JSO_TYPE_ARRAY) {
+		// Iterate through positions to check composition for all types except array and object
 		jso_schema_validation_stack_layer_iterator_start(stack, &iterator);
 		while ((pos = jso_schema_validation_stack_layer_iterator_next(stack, &iterator))) {
 			if (pos->validation_result == JSO_SCHEMA_VALIDATION_VALID) {
 				if (instance_type == JSO_TYPE_OBJECT) {
+					// For object, do the pre validation that adds dependency schemas
 					if (JSO_SCHEMA_VALUE_TYPE_P(pos->current_value) == JSO_SCHEMA_VALUE_TYPE_OBJECT
 							&& jso_schema_validation_object_pre_value(stack, pos)
 									!= JSO_SCHEMA_VALIDATION_VALID) {
@@ -223,20 +247,22 @@ JSO_API jso_rc jso_schema_validation_stream_value(
 			}
 		}
 	}
+	// Now the reverse iteration is done and each applicable value validated. The reverse order
+	// is done so parent position is validate after children.
 	jso_schema_validation_stack_layer_reverse_iterator_start(stack, &iterator);
 	while ((pos = jso_schema_validation_stack_layer_reverse_iterator_next(stack, &iterator))) {
-		if (!pos->is_final_validation_result) {
-			if (pos->validation_result == JSO_SCHEMA_VALIDATION_VALID
-					&& (pos->composition_type != JSO_SCHEMA_VALIDATION_COMPOSITION_ANY
-							|| !pos->parent->any_of_valid)) {
-				pos->validation_result = jso_schema_validation_value(schema, pos, instance);
-				if (jso_schema_validation_stream_should_terminate(schema, pos)) {
-					return JSO_FAILURE;
-				}
+		if (!pos->is_final_validation_result
+				&& pos->validation_result == JSO_SCHEMA_VALIDATION_VALID
+				&& (pos->composition_type != JSO_SCHEMA_VALIDATION_COMPOSITION_ANY
+						|| !pos->parent->any_of_valid)) {
+			pos->validation_result = jso_schema_validation_value(schema, pos, instance);
+			if (jso_schema_validation_stream_should_terminate(schema, pos)) {
+				return JSO_FAILURE;
 			}
 		}
 		jso_schema_validation_result_propagate(schema, pos);
 	}
+	// Reset the layer to the last separator.
 	jso_schema_validation_stack_layer_remove(stack);
 
 	return JSO_SUCCESS;
