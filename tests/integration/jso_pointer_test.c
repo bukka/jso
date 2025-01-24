11082b9f9e277c2e6e9dc0f111adaf73b4bb4382
Jakub Zelenka <bukka@php.net>
2025-01-24 17:03:02 +0100

    Integration test pointer resolving

--- tests/integration/jso_pointer_test.c
+++ tests/integration/jso_pointer_test.c
@@ -22,6 +22,7 @@
  */
 
 #include "../../src/jso_pointer.h"
+#include "../../src/jso_builder.h"
 #include "../../src/jso.h"
 
 #include <stdarg.h>
@@ -121,19 +122,127 @@ static void test_jso_pointer_create_with_invalid_escape_end(void **state)
 	jso_pointer_free(pointer);
 }
 
-/* A test evaluating object. */
-static void test_jso_pointer_evaluate_object(void **state)
+/* A test for point resolving. */
+static void test_jso_pointer_resolve(void **state)
 {
 	(void) state; /* unused */
 
-	jso_string *str = jso_string_create_from_cstr("/invalid~/end");
-	jso_pointer *pointer = jso_pointer_create(str);
+	jso_builder builder;
+	jso_builder_init(&builder);
 
-	assert_non_null(pointer);
-	assert_int_equal(JSO_POINTER_ERROR_TYPE(pointer), JSO_POINTER_ERROR_INVALID_FORMAT);
+	// build schema
+	jso_builder_object_start(&builder);
+	jso_builder_object_add_cstr(&builder, "key_str", "str1");
+	jso_builder_object_add_int(&builder, "key_int", 11);
+	jso_builder_object_add_object_start(&builder, "key_obj");
+	jso_builder_object_add_int(&builder, "nested_key_int", 111);
+	jso_builder_object_add_object_start(&builder, "nested_key_obj");
+	jso_builder_object_add_cstr(&builder, "deep_key_str", "str_deep");
+	jso_builder_object_end(&builder); // nested_key_obj
+	jso_builder_object_add_array_start(&builder, "nested_key_arr");
+	jso_builder_array_add_int(&builder, 112);
+	jso_builder_array_add_cstr(&builder, "data");
+	jso_builder_array_add_object_start(&builder);
+	jso_builder_object_add_cstr(&builder, "arr_key", "arr_val");
+	jso_builder_object_end(&builder); // arr boj
+	jso_builder_array_add_array_start(&builder);
+	jso_builder_array_add_int(&builder, 114);
+	jso_builder_array_end(&builder); // nested arr
+	jso_builder_array_end(&builder); // arra
+	jso_builder_object_end(&builder); // key_obj
+	jso_builder_object_end(&builder); // root
 
+	jso_value *root = jso_builder_get_value(&builder);
+	jso_value *value;
+	jso_string *str;
+	jso_pointer *pointer;
+
+	str = jso_string_create_from_cstr("/key_str");
+	pointer = jso_pointer_create(str);
+	assert_int_equal(JSO_SUCCESS, jso_pointer_resolve(pointer, root, &value));
+	assert_int_equal(JSO_TYPE_STRING, JSO_TYPE_P(value));
+	assert_true(jso_string_equals_to_cstr(JSO_STR_P(value), "str1"));
 	jso_string_free(str);
 	jso_pointer_free(pointer);
+
+	str = jso_string_create_from_cstr("/key_int");
+	pointer = jso_pointer_create(str);
+	assert_int_equal(JSO_SUCCESS, jso_pointer_resolve(pointer, root, &value));
+	assert_int_equal(JSO_TYPE_INT, JSO_TYPE_P(value));
+	assert_int_equal(11, JSO_IVAL_P(value));
+	jso_string_free(str);
+	jso_pointer_free(pointer);
+
+	str = jso_string_create_from_cstr("/key_obj/nested_key_int");
+	pointer = jso_pointer_create(str);
+	assert_int_equal(JSO_SUCCESS, jso_pointer_resolve(pointer, root, &value));
+	assert_int_equal(JSO_TYPE_INT, JSO_TYPE_P(value));
+	assert_int_equal(111, JSO_IVAL_P(value));
+	jso_string_free(str);
+	jso_pointer_free(pointer);
+
+	str = jso_string_create_from_cstr("/key_obj/nested_key_obj/deep_key_str");
+	pointer = jso_pointer_create(str);
+	assert_int_equal(JSO_SUCCESS, jso_pointer_resolve(pointer, root, &value));
+	assert_int_equal(JSO_TYPE_STRING, JSO_TYPE_P(value));
+	assert_true(jso_string_equals_to_cstr(JSO_STR_P(value), "str_deep"));
+	jso_string_free(str);
+	jso_pointer_free(pointer);
+
+	str = jso_string_create_from_cstr("/key_obj/nested_key_arr/0");
+	pointer = jso_pointer_create(str);
+	assert_int_equal(JSO_SUCCESS, jso_pointer_resolve(pointer, root, &value));
+	assert_int_equal(JSO_TYPE_INT, JSO_TYPE_P(value));
+	assert_int_equal(112, JSO_IVAL_P(value));
+	jso_string_free(str);
+	jso_pointer_free(pointer);
+
+	str = jso_string_create_from_cstr("/key_obj/nested_key_arr/1");
+	pointer = jso_pointer_create(str);
+	assert_int_equal(JSO_SUCCESS, jso_pointer_resolve(pointer, root, &value));
+	assert_int_equal(JSO_TYPE_STRING, JSO_TYPE_P(value));
+	assert_true(jso_string_equals_to_cstr(JSO_STR_P(value), "data"));
+	jso_string_free(str);
+	jso_pointer_free(pointer);
+
+	str = jso_string_create_from_cstr("/key_obj/nested_key_arr/2/arr_key");
+	pointer = jso_pointer_create(str);
+	assert_int_equal(JSO_SUCCESS, jso_pointer_resolve(pointer, root, &value));
+	assert_int_equal(JSO_TYPE_STRING, JSO_TYPE_P(value));
+	assert_true(jso_string_equals_to_cstr(JSO_STR_P(value), "arr_val"));
+	jso_string_free(str);
+	jso_pointer_free(pointer);
+
+	str = jso_string_create_from_cstr("/key_obj/nested_key_arr/3/0");
+	pointer = jso_pointer_create(str);
+	assert_int_equal(JSO_SUCCESS, jso_pointer_resolve(pointer, root, &value));
+	assert_int_equal(JSO_TYPE_INT, JSO_TYPE_P(value));
+	assert_int_equal(114, JSO_IVAL_P(value));
+	jso_string_free(str);
+	jso_pointer_free(pointer);
+
+	str = jso_string_create_from_cstr("/key_unknown");
+	pointer = jso_pointer_create(str);
+	assert_int_equal(JSO_FAILURE, jso_pointer_resolve(pointer, root, &value));
+	assert_int_equal(JSO_POINTER_ERROR_NOT_FOUND, JSO_POINTER_ERROR_TYPE(pointer));
+	jso_string_free(str);
+	jso_pointer_free(pointer);
+
+	str = jso_string_create_from_cstr("/key_obj/nested_key_arr/8");
+	pointer = jso_pointer_create(str);
+	assert_int_equal(JSO_FAILURE, jso_pointer_resolve(pointer, root, &value));
+	assert_int_equal(JSO_POINTER_ERROR_NOT_FOUND, JSO_POINTER_ERROR_TYPE(pointer));
+	jso_string_free(str);
+	jso_pointer_free(pointer);
+
+	str = jso_string_create_from_cstr("/key_obj/nested_key_arr/val");
+	pointer = jso_pointer_create(str);
+	assert_int_equal(JSO_FAILURE, jso_pointer_resolve(pointer, root, &value));
+	assert_int_equal(JSO_POINTER_ERROR_INVALID_ARRAY_INDEX, JSO_POINTER_ERROR_TYPE(pointer));
+	jso_string_free(str);
+	jso_pointer_free(pointer);
+
+	jso_builder_clear_all(&builder);
 }
 
 int main(void)
@@ -144,6 +253,7 @@ int main(void)
 		cmocka_unit_test(test_jso_pointer_create_with_valid_mixed_escapes),
 		cmocka_unit_test(test_jso_pointer_create_with_invalid_escape_number),
 		cmocka_unit_test(test_jso_pointer_create_with_invalid_escape_end),
+		cmocka_unit_test(test_jso_pointer_resolve),
 	};
 
 	return cmocka_run_group_tests(tests, NULL, NULL);
