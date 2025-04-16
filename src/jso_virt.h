64cb6c33bc9a5e5811b89cf58432ce12ed2b6d38
Jakub Zelenka <bukka@php.net>
2025-04-16 17:14:37 +0200

    Document jso_virt API

--- src/jso_virt.h
+++ src/jso_virt.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012-2025 Jakub Zelenka. All rights reserved.
+ * Copyright (c) 2025 Jakub Zelenka. All rights reserved.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a
  * copy of this software and associated documentation files (the "Software"),
@@ -23,7 +23,9 @@
 
 /**
  * @file jso_virt.h
- * @brief JSO virtual API that can be replace by embedder
+ * @brief JSO virtual API that can be replaced by embedders
+ *
+ * This is minimal API that can be overwritten by application that embeds JSO.
  */
 
 #ifndef JSO_VIRT_H
@@ -36,43 +38,102 @@
 #include "jso_value.h"
 
 /* types */
+
+/**
+ * @brief value type
+ */
 #define jso_virt_value jso_value
+
+/**
+ * @brief string type
+ */
 #define jso_virt_string jso_string
+
+/**
+ * @brief array type
+ */
 #define jso_virt_array jso_array
+
+/**
+ * @brief object type
+ */
 #define jso_virt_object jso_object
 
 /* value */
 
+/**
+ * Get the JSO type.
+ *
+ * @param val virtual value
+ * @return value type of @ref jso_value_type
+ */
 static inline jso_value_type jso_virt_value_type(jso_virt_value *val)
 {
 	return JSO_TYPE_P(val);
 }
 
+/**
+ * Get integer value of the supplied virtual value.
+ *
+ * @param val virtual value
+ * @return integer value
+ */
 static inline jso_int jso_virt_value_int(jso_virt_value *val)
 {
 	return JSO_IVAL_P(val);
 }
 
+/**
+ * Get double value of the supplied virtual value.
+ *
+ * @param val virtual value
+ * @return double value
+ */
 static inline jso_double jso_virt_value_double(jso_virt_value *val)
 {
 	return JSO_DVAL_P(val);
 }
 
+/**
+ * Get string value of the supplied virtual value.
+ *
+ * @param val virtual value
+ * @return string value
+ */
 static inline jso_virt_string *jso_virt_value_string(jso_virt_value *val)
 {
 	return JSO_STR_P(val);
 }
 
+/**
+ * Get array value of the supplied virtual value.
+ *
+ * @param val virtual value
+ * @return array value
+ */
 static inline jso_virt_array *jso_virt_value_array(jso_virt_value *val)
 {
 	return JSO_ARRVAL_P(val);
 }
 
+/**
+ * Get object value of the supplied virtual value.
+ *
+ * @param val virtual value
+ * @return integer value
+ */
 static inline jso_virt_object *jso_virt_value_object(jso_virt_value *val)
 {
 	return JSO_OBJVAL_P(val);
 }
 
+/**
+ * Check if virtual value is equal to JSO value.
+ *
+ * @param vval virtual value
+ * @param val JSO value
+ * @return true if the values are equal, otherwise false
+ */
 static inline bool jso_virt_value_equals(jso_virt_value *vval, jso_value *val)
 {
 	return jso_value_equals(vval, val);
@@ -80,11 +141,23 @@ static inline bool jso_virt_value_equals(jso_virt_value *vval, jso_value *val)
 
 /* string */
 
+/**
+ * Get C string value of the string.
+ *
+ * @param str virtual string
+ * @return C string value of the string
+ */
 static inline const char *jso_virt_string_val(jso_virt_string *str)
 {
 	return JSO_STRING_CSTR_VAL(str);
 }
 
+/**
+ * Get length of the string.
+ *
+ * @param str virtual string
+ * @return string length
+ */
 static inline size_t jso_virt_string_len(jso_virt_string *str)
 {
 	return JSO_STRING_LEN(str);
@@ -95,11 +168,23 @@ static inline size_t jso_virt_string_len(jso_virt_string *str)
 #define JSO_VIRT_ARRAY_FOREACH JSO_ARRAY_FOREACH
 #define JSO_VIRT_ARRAY_FOREACH_END JSO_ARRAY_FOREACH_END
 
+/**
+ * Get length of the array.
+ *
+ * @param arr virtual array
+ * @return array length
+ */
 static inline size_t jso_virt_array_len(jso_virt_array *arr)
 {
 	return JSO_ARRAY_LEN(arr);
 }
 
+/**
+ * Check if array contains only unique elements.
+ *
+ * @param arr virtual array
+ * @return true if the array is unique, otherwise false
+ */
 static inline bool jso_virt_array_is_unique(jso_virt_array *arr)
 {
 	return jso_array_is_unique(arr);
@@ -110,11 +195,24 @@ static inline bool jso_virt_array_is_unique(jso_virt_array *arr)
 #define JSO_VIRT_OBJECT_FOREACH JSO_OBJECT_FOREACH
 #define JSO_VIRT_OBJECT_FOREACH_END JSO_OBJECT_FOREACH_END
 
+/**
+ * Check if the key is in object.
+ *
+ * @param obj virtual object
+ * @param key key to check
+ * @return true if the key is in object, otherwise false
+ */
 static inline bool jso_virt_object_has_str_key(jso_virt_object *obj, jso_string *key)
 {
 	return jso_object_has(obj, key);
 }
 
+/**
+ * Get length of the object.
+ *
+ * @param obj virtual object
+ * @return object length
+ */
 static inline size_t jso_virt_object_count(jso_virt_object *obj)
 {
 	return JSO_OBJECT_COUNT(obj);
